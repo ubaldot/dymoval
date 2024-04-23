cc57af46fdad8d4515bcdc05ddb189d9d7779d47
Ubaldo Tiberi <ubaldo.tiberi@volvo.com>
2024-04-23 14:49:04 +0200

    Adjusted tutorial

--- src/dymoval_tutorial/DCMotorModel.py
+++ src/dymoval_tutorial/DCMotorModel.py
@@ -12,27 +12,27 @@ b = 1e-4
 K = 0.1
 
 # Working values
-L = 0.9e-3
-R = 1.1
-J = 5e-5
-b = 0.9e-4
-K = 0.12
+L = 1e-3
+R = 1.08
+J = 5.5e-5
+b = 1.5e-4
+K = 0.103
 
-# L = 1e-3
-# R = 1.8
-# J = 4.5e-5
-# b = 0.9e-4
-# K = 0.12
 
 # define motor state variable model
-A = np.array([[-R / L, 0, -K / L], [0, 0, 1], [K / J, 0, -b / J]]).round(
-    dmv.NUM_DECIMALS
-)
-B = np.array([[1.0 / L], [0], [0]]).round(dmv.NUM_DECIMALS)
-C = np.array([[1.0, 0, 0], [0, 1.0, 0], [0, 0, 9.5493]]).round(
-    dmv.NUM_DECIMALS
-)  # Convert units from rad/s to RPM
-D = np.array([[0], [0], [0]]).round(dmv.NUM_DECIMALS)
+# A = np.array([[-R / L, 0, -K / L], [0, 0, 1], [K / J, 0, -b / J]]).round(
+#     dmv.NUM_DECIMALS
+# )
+# B = np.array([[1.0 / L], [0], [0]]).round(dmv.NUM_DECIMALS)
+# C = np.array([[1.0, 0, 0], [0, 1.0, 0], [0, 0, 9.5493]]).round(
+#     dmv.NUM_DECIMALS
+# )  # Convert units from rad/s to RPM
+# D = np.array([[0], [0], [0]]).round(dmv.NUM_DECIMALS)
+
+A = np.array([[-R / L, 0, -K / L], [0, 0, 1], [K / J, 0, -b / J]])
+B = np.array([[1.0 / L], [0], [0]])
+C = np.array([[1.0, 0, 0], [0, 1.0, 0], [0, 0, 9.5493]])
+D = np.array([[0], [0], [0]])
 
 DCMotor_ct = ct.ss(A, B, C, D)
 
