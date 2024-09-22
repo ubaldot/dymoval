582786590e5bb7b484407904e6df06bf225e272f
Ubaldo Tiberi <ubaldo.tiberi@gmail.com>
2024-09-22 09:42:21 +0200

    Blacked

--- src/dymoval/config.py
+++ src/dymoval/config.py
@@ -49,6 +49,4 @@ SPECTRUM_KIND: list[Spectrum_type] = list(typing.get_args(Spectrum_type))
 Allowed_keys_type = Literal[
     "name", "values", "signal_unit", "sampling_period", "time_unit"
 ]
-SIGNAL_KEYS: list[Allowed_keys_type] = list(
-    typing.get_args(Allowed_keys_type)
-)
+SIGNAL_KEYS: list[Allowed_keys_type] = list(typing.get_args(Allowed_keys_type))
