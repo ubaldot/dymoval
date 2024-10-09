8909cbcd50469205627514b712b592cc374bd269
Ubaldo Tiberi <ubaldo.tiberi@volvo.com>
2024-10-09 11:31:52 +0200

    test2

--- src/dymoval/validation.py
+++ src/dymoval/validation.py
@@ -1,5 +1,7 @@
 """Module containing everything related to validation."""
 
+# TODO: This is for testing Git status
+
 # The following is needed when there are methods that return instance of the
 # class itself.
 # TODO If you remove python 3.10 remove typing_extensions as Self in typing is
